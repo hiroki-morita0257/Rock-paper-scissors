accca6557245a6c58fbe01b405f8ddbad2db478f
hiroki-morita0257 <s1f102502575@iniad.org>
2025-08-26 19:06:54 +0900

    ルーレットのストップ時の処理が完成
    ルーレットを止めると同時にcomputer_handを判定し表示を固定する。

--- main.js
+++ main.js
@@ -12,9 +12,10 @@ const select_pa = document.getElementById("answer_select_pa");
 
 const result_win = document.getElementById("result_win");
 const result_lose = document.getElementById("result_lose");
+const GU = "gu";
 
 start_button.addEventListener("click", function () {
-  ready_message.classList.toggle("start");
+  ready_message.classList.remove("ready");
   condition = 1;
 });
 
@@ -22,61 +23,59 @@ const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function one_rotation() {
   computer_gu.classList.add("gu");
-  await wait(150);
+  await wait(100);
   computer_gu.classList.remove("gu");
 
   computer_cho.classList.add("cho");
-  await wait(150);
+  await wait(100);
   computer_cho.classList.remove("cho");
 
   computer_pa.classList.add("pa");
-  await wait(150);
+  await wait(100);
   computer_pa.classList.remove("pa");
 }
-
-// let gu_a = setInterval(function () {
-// }, 1000);
-
-// let gu_r = setInterval(function () {
-// }, 1000);
-// let cho_a = setInterval(function () {
-// }, 100);
-
-// let cho_r = setInterval(function () {
-// }, 100);
-// let pa_a = setInterval(function () {
-// }, 100);
-
-// let pa_r = setInterval(function () {
-// }, 100);
 
 let roulette;
 function start_roulette() {
   roulette = setInterval(function () {
     one_rotation();
-  }, 451);
+  }, 301);
 }
 function stop_roulette() {
   clearInterval(roulette);
 }
 
-// const roulette = setInterval(function () {
-//     one_rotation();
-//   }, 451);
+let computer_hand;
+function judge() {
+  if (computer_gu.classList.contains("gu")) {
+    computer_hand = 0;
+  } else if (computer_cho.classList.contains("cho")) {
+    computer_hand = 1;
+  } else if (computer_pa.classList.contains("pa")) {
+    computer_hand = 2;
+  }
+  console.log(computer_hand);
+}
+function inview_computer_hand() {
+  if (computer_hand === 0) {
+    computer_gu.classList.remove("gu");
+    computer_gu.classList.add("gu");
+  } else if (computer_hand === 1) {
+    computer_cho.classList.remove("cho");
+    computer_cho.classList.add("cho");
+  } else if (computer_hand === 2) {
+    computer_pa.classList.remove("pa");
+    computer_pa.classList.add("pa");
+  }
+  console.log("表示");
+}
 
-// for (let i = 0; i < 5; i++) {
-//   console.log("実行");
-//   rulet();
-// }
-
-// while (false) {
-//   rulet();
-//   // setInterval(function () {
-//   //   rulet()
-//   // }, )
-// }
-
-function Judge(hand) {}
+async function stop_routine() {
+  judge();
+  stop_roulette();
+  await wait(100);
+  inview_computer_hand();
+}
 
 select_gu.addEventListener("click", function () {});
 select_cho.addEventListener("click", function () {});
