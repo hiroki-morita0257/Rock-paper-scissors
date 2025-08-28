986ca264fd6284eae075e4513e70ce269eae1ca8
hiroki-morita0257 <s1f102502575@iniad.org>
2025-08-28 15:12:30 +0900

    リファクタリングの完了と、playerの手を決めたら変更できなくするロジックを実装しました。
    複数回行う動作や、付けはずしするクラス名などを全て変数化、関数化し、置き換えました。
    
    playerが手を決めた後にも、グーチョキパーのボタンが押せてしまうため、勝敗の結果が重複して表示されてしまうバグがあったので、こちらも、一度勝敗が決したらボタンを無効化することで対処しました。
    この際、選択した手を複数回押す分には問題がないため、選択した手以外のボタンを無効化することで、選択した後にも、自分がどの手を選択したのかがわかるようにしました。

--- main.js
+++ main.js
@@ -14,27 +14,119 @@ const result_win = document.getElementById("result_win");
 const result_lose = document.getElementById("result_lose");
 const result_draw = document.getElementById("result_draw");
 const error_message = document.getElementById("error_message");
-const GU = "gu";
+
+const Started = "started";
+const Gu = "gu";
+const Choki = "cho";
+const Pa = "pa";
+const Win = "win";
+const Lose = "lose";
+const Draw = "draw";
+const Error_messe = "error";
+
+function Add_Started() {
+  ready_message.classList.add(Started);
+}
+function Remove_Started() {
+  ready_message.classList.remove(Started);
+}
+
+function Add_Gu() {
+  computer_gu.classList.add(Gu);
+}
+function Remove_Gu() {
+  computer_gu.classList.remove(Gu);
+}
+function Add_Choki() {
+  computer_cho.classList.add(Choki);
+}
+function Remove_Choki() {
+  computer_cho.classList.remove(Choki);
+}
+function Add_Pa() {
+  computer_pa.classList.add(Pa);
+}
+function Remove_Pa() {
+  computer_pa.classList.remove(Pa);
+}
+function Contain_Gu() {
+  return computer_gu.classList.contains(Gu);
+}
+function Contain_Choki() {
+  return computer_cho.classList.contains(Choki);
+}
+function Contain_Pa() {
+  return computer_pa.classList.contains(Pa);
+}
+
+function Add_Win() {
+  result_win.classList.add(Win);
+}
+function Remove_Win() {
+  result_win.classList.remove(Win);
+}
+function Add_Lose() {
+  result_lose.classList.add(Lose);
+}
+function Remove_Lose() {
+  result_lose.classList.remove(Lose);
+}
+function Add_Draw() {
+  result_draw.classList.add(Draw);
+}
+function Remove_Draw() {
+  result_draw.classList.remove(Draw);
+}
+function Add_Error() {
+  error_message.classList.add(Error_messe);
+}
+function Remove_Error() {
+  error_message.classList.remove(Error_messe);
+}
+function Start_Add_disabled() {
+  start_button.disabled = "disabled";
+}
+function Start_Remove_Disabled() {
+  start_button.disabled = "";
+}
+function End_Add_disabled_Gu() {
+  select_gu.disabled = "disabled";
+}
+function End_Add_disabled_Choki() {
+  select_cho.disabled = "disabled";
+}
+function End_Add_disabled_Pa() {
+  select_pa.disabled = "disabled";
+}
+function End_Remove_Disabled_Gu() {
+  select_gu.disabled = "";
+}
+function End_Remove_disabled_Choki() {
+  select_cho.disabled = "";
+}
+function End_Remove_disabled_Pa() {
+  select_pa.disabled = "";
+}
 
 start_button.addEventListener("click", function () {
-  ready_message.classList.add("started");
+  Add_Started();
   condition = 1;
 });
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function one_rotation() {
-  computer_gu.classList.add("gu");
+  Add_Gu();
   await wait(70);
-  computer_gu.classList.remove("gu");
+  Remove_Gu();
 
-  computer_cho.classList.add("cho");
+  Add_Choki();
   await wait(70);
-  computer_cho.classList.remove("cho");
+  Remove_Choki();
 
-  computer_pa.classList.add("pa");
+  Add_Pa();
   await wait(70);
-  computer_pa.classList.remove("pa");
+  Remove_Pa();
 }
 
 let rouletteID;
@@ -42,51 +134,45 @@ function start_roulette() {
   rouletteID = setInterval(function () {
     one_rotation();
   }, 211);
-  result_win.classList.remove("win");
-  result_lose.classList.remove("lose");
-  result_draw.classList.remove("draw");
-  error_message.classList.remove("error");
+  Remove_Win();
+  Remove_Lose();
+  Remove_Draw();
+  Remove_Error();
   // start_button.classList.remove("waiting");
-  start_button.disabled = "disabled";
+  Start_Add_disabled();
+  End_Remove_Disabled_Gu();
+  End_Remove_disabled_Choki();
+  End_Remove_disabled_Pa();
 }
 function stop_roulette() {
   clearInterval(rouletteID);
   // start_button.classList.add("waiting");
-  start_button.disabled = "";
+  Start_Remove_Disabled();
 }
 
 // let computer_hand;
 function detect() {
-  if (computer_gu.classList.contains("gu")) {
+  if (Contain_Gu()) {
     return 0;
-  } else if (computer_cho.classList.contains("cho")) {
+  } else if (Contain_Choki()) {
     return 1;
-  } else if (computer_pa.classList.contains("pa")) {
+  } else if (Contain_Pa()) {
     return 2;
   }
   // console.log(computer_hand);
 }
 function inview_computer_hand(computer_hand) {
-  computer_gu.classList.remove("gu");
-  computer_cho.classList.remove("cho");
-  computer_pa.classList.remove("pa");
+  Remove_Gu();
+  Remove_Choki();
+  Remove_Pa();
   if (computer_hand === 0) {
-    computer_gu.classList.add("gu");
+    Add_Gu();
   } else if (computer_hand === 1) {
-    computer_cho.classList.add("cho");
+    Add_Choki();
   } else if (computer_hand === 2) {
-    computer_pa.classList.add("pa");
+    Add_Pa();
   }
   console.log("com手を表示");
-}
-
-async function stop_routine() {
-  computer_hand = detect();
-  console.log(computer_hand);
-  stop_roulette();
-  await wait(100);
-  inview_computer_hand(computer_hand);
-  console.log("ストップルーティーン終了");
 }
 
 function judge(select_hand) {
@@ -109,23 +195,45 @@ function judge(select_hand) {
   ) {
     return 2;
   } else {
-    error_message.classList.add("error");
+    Add_Error();
   }
-  console.log(judge_result);
+  // console.log(judge_result);
 }
 function inview_result(judge_result) {
   if (judge_result === 0) {
-    result_win.classList.add("win");
+    Add_Win();
   } else if (judge_result === 1) {
-    result_lose.classList.add("lose");
+    Add_Lose();
   } else if (judge_result === 2) {
-    result_draw.classList.add("draw");
+    Add_Draw();
   }
   console.log("勝敗を表示");
+}
+function Disable_Others(player_hand) {
+  if (player_hand === 0) {
+    End_Add_disabled_Choki();
+    End_Add_disabled_Pa();
+  } else if (player_hand === 1) {
+    End_Add_disabled_Gu();
+    End_Add_disabled_Pa();
+  } else if (player_hand === 2) {
+    End_Add_disabled_Gu();
+    End_Add_disabled_Choki();
+  }
+}
+
+async function stop_routine() {
+  computer_hand = detect();
+  console.log(computer_hand);
+  stop_roulette();
+  await wait(100);
+  inview_computer_hand(computer_hand);
+  console.log("ストップルーティーン終了");
 }
 
 function jyanken_judge(player_hand) {
   stop_routine();
+  Disable_Others(player_hand);
   const judge_result = judge(player_hand);
   console.log(judge_result);
   inview_result(judge_result);
